599e91e138c8da947294d0176be7094b60f5ed79
alex <alexey.myakinky@dev4hana.ru>
2024-11-27 23:05:48 +0100

    change graph logic
    when we do not search for all numbers, return the whole graph we have built.
    with respect to limit

--- test/search.test.js
+++ test/search.test.js
@@ -9,9 +9,33 @@ test('limit not enough', () => {
     expect(() => search(3, false, 5)()).toThrow("We're stuck! Consider increasing LIM");
 })
 
-test('limit enough for simple path', () => {
-    const result = search(853, false, 100)
-    expect(result).toEqual([{ val: 1, parent: 1 }, { val: 5, parent: 1 }, { val: 853, parent: 5 }])
+test('path to 11 with all the nodes we generated', () => {
+    const result = search(11, false, 500*11)
+    expect(result).toEqual([
+        { val: 1, parent: 1 }, { val: 3, parent: 5 }, { val: 5, parent: 1 },
+        { val: 11, parent: 17 }, { val: 13, parent: 5 }, { val: 17, parent: 13 },
+        { val: 21, parent: 1 }, { val: 53, parent: 5 }, { val: 69, parent: 13 },
+        { val: 85, parent: 1 }, { val: 113, parent: 85 }, { val: 213, parent: 5 },
+        { val: 341, parent: 1 }, { val: 1365, parent: 1 }
+    ])
+})
+
+test('path to 11 with some nodes skipped due to limit', () => {
+    const result = search(11, false, 100)
+    expect(result).toEqual([
+        { val: 1, parent: 1 }, { val: 3, parent: 5 }, { val: 5, parent: 1 },
+        { val: 11, parent: 17 }, { val: 13, parent: 5 }, { val: 17, parent: 13 },
+        { val: 21, parent: 1 }, { val: 53, parent: 5 }, { val: 69, parent: 13 },
+        { val: 85, parent: 1 }
+    ])
+})
+
+test('path to 9 where default 500*9 limit is less than max potential number we generate', () => {
+    const resultAll = search(9, false, 500*100)
+    expect(resultAll.length).toBe(32)
+
+    const resultSkipped = search(9, false, 500*9)
+    expect(resultSkipped.length).toBe(30)
 })
 
 test('simple tree up to 7', () => {
