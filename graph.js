02bed92599eaa2e69a369484cdb8e959eed891e4
alex <alexey.myakinky@dev4hana.ru>
2024-11-28 01:38:10 +0100

    added function to run 3n+1 sequence
    instead of building the graph up from root
    but it is boring

--- graph.js
+++ graph.js
@@ -4,8 +4,9 @@ try {
     console.error(e.message) // not a critical error though
 }
 
-const { search, getPrimes } = require("./lib")
+const { trace, search, getPrimes } = require("./lib")
 
+const TRACE = +process.env.TRACE // magic parameter to run sequence for a number instead of searching
 const SEARCH = +process.env.SEARCH || 7 // target number
 const LIM = +process.env.LIM || 500 * SEARCH // max value to be added to graph
 const ALL = !!process.env.ALL // also search for all numbers below target
@@ -14,10 +15,12 @@ const SKIP = !!process.env.SKIP // only return nodes that lead to search targets
 try {
     console.log("graph { \n")
 
-    const result = search(SEARCH, ALL, LIM, SKIP)
+    const result = TRACE ? trace(TRACE, ALL) : search(SEARCH, ALL, LIM, SKIP)
 
+    const now=Date.now()
     getPrimes(result.map(v => v.val)).forEach(p => console.log(`${p} [color=green]`))
     console.log("\n")
+    console.error(`getPrimes took ${(Date.now()-now)/1000}s`)
 
     result.forEach(v => console.log(`${v.val} -- ${v.parent}`))
 
