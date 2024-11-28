8bca32f84d532410bcbb81fb6eb7eeb9a444c6be
alex <alexey.myakinky@dev4hana.ru>
2024-11-28 14:28:16 +0100

    lim and tests to trace
    its so much faster it is boring..

--- graph.js
+++ graph.js
@@ -8,14 +8,14 @@ const { trace, search, getPrimes } = require("./lib")
 
 const TRACE = +process.env.TRACE // magic parameter to run sequence for a number instead of searching
 const SEARCH = +process.env.SEARCH || 7 // target number
-const LIM = +process.env.LIM || 500 * SEARCH // max value to be added to graph
+const LIM = +process.env.LIM || 500 * (TRACE || SEARCH) // max value to be added to graph
 const ALL = !!process.env.ALL // also search for all numbers below target
 const SKIP = !!process.env.SKIP // only return nodes that lead to search targets
 
 try {
     console.log("graph { \n")
 
-    const result = TRACE ? trace(TRACE, ALL) : search(SEARCH, ALL, LIM, SKIP)
+    const result = TRACE ? trace(TRACE, ALL, LIM) : search(SEARCH, ALL, LIM, SKIP)
 
     const now=Date.now()
     getPrimes(result.map(v => v.val)).forEach(p => console.log(`${p} [color=green]`))
--- test/search.test.js
+++ test/search.test.js
@@ -1,4 +1,4 @@
-const { search } = require("../lib")
+const { search, trace } = require("../lib")
 
 test('simplest real case', () => {
     const result = search(5, false, 0)
@@ -6,11 +6,13 @@ test('simplest real case', () => {
 })
 
 test('limit not enough', () => {
-    expect(() => search(3, false, 5)()).toThrow("We're stuck! Consider increasing LIM");
+    const result = search(3, false, 5) // ok
+    expect(result.length).toBe(3)
+    expect(() => search(3, false, 3)()).toThrow("We're stuck! Consider increasing LIM");
 })
 
 test('path to 11 with all the nodes we generated', () => {
-    const result = search(11, false, 500*11)
+    const result = search(11, false, 500 * 11)
     expect(result).toEqual([
         { val: 1, parent: 1 }, { val: 3, parent: 5 }, { val: 5, parent: 1 },
         { val: 11, parent: 17 }, { val: 13, parent: 5 }, { val: 17, parent: 13 },
@@ -31,10 +33,10 @@ test('path to 11 with some nodes skipped due to limit', () => {
 })
 
 test('path to 9 where default 500*9 limit is less than max potential number we generate', () => {
-    const resultAll = search(9, false, 500*100)
+    const resultAll = search(9, false, 500 * 100)
     expect(resultAll.length).toBe(32)
 
-    const resultSkipped = search(9, false, 500*9)
+    const resultSkipped = search(9, false, 500 * 9)
     expect(resultSkipped.length).toBe(30)
 })
 
@@ -50,6 +52,25 @@ test('bigger tree up to 17', () => {
     const result = search(17, true, 100, true)
     expect(result).toEqual([
         { val: 1, parent: 1 }, { val: 3, parent: 5 }, { val: 5, parent: 1 }, { val: 7, parent: 11 },
-        { val: 9, parent: 7 }, { val: 11, parent: 17 }, { val: 13, parent: 5 }, { val: 15, parent: 23 }, 
+        { val: 9, parent: 7 }, { val: 11, parent: 17 }, { val: 13, parent: 5 }, { val: 15, parent: 23 },
         { val: 17, parent: 13 }, { val: 23, parent: 35 }, { val: 35, parent: 53 }, { val: 53, parent: 5 }
-    ])})
+    ])
+})
+
+test('trace and search provide same path up to 127 single target', () => {
+    const resultUp = search(127, false, 500*127, true)
+    const resultDown = trace(127, false)
+    expect(resultUp).toEqual(resultDown)
+})
+
+test('trace and search provide same tree up to 127 ', () => {
+    const resultUp = search(127, true, 500*127, true)
+    const resultDown = trace(127, true)
+    expect(resultUp).toEqual(resultDown)
+})
+
+test('limit not enough for trace', () => {
+    const result = trace(3, false, 5) // ok
+    expect(result.length).toBe(3)
+    expect(() => trace(3, false, 3)()).toThrow("We're stuck! Consider increasing LIM");
+})
